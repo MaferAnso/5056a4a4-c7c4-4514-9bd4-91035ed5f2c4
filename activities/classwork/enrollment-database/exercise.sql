825ce1025f9ab7d6febdb276f16f4438275debd8
MaferAnso <mafer.ansoleaga@gmail.com>
2019-11-11 16:19:09 -0600

    Tarea

--- activities/classwork/enrollment-database/exercise.sql
+++ activities/classwork/enrollment-database/exercise.sql
@@ -24,10 +24,50 @@
  DESCRIPTION: Write a valid SQL query to solve each problem.
  */
 
+SECTION: Individual exercises
+ DESCRIPTION: Write a valid SQL query to solve each problem.
+ */
+
 -- A) Count the number of courses per department
 SELECT
     count(department)
 FROM
+    course
+;
+
+-- B) How many male/female students were accepted?
+SELECT
+    s.gender,
+    count(s.gender)
+FROM
+    student s INNER JOIN enrollment e ON s.id = e.student_id -- INNER JOIN = product cartesiano -- s=student; e=enrollment
+WHERE
+    e.approved = 1
+GROUP BY
+    s.gender
+;
+
+-- C) How many students were accepted per course?
+SELECT
+    c.name,
+    count(c.name)
+FROM
+     enrollment e INNER JOIN course c ON e.course_id = c.id
+WHERE
+    e.approved = 1
+GROUP BY
+    c.name
+;
+
+-- D) What's the average age and gpa per course?
+SELECT
+    AVG(s.gpa) AS "Average of student's GPA",
+    AVG(s.age) AS "Average of student's age"
+FROM
+    student s INNER JOIN enrollment e ON s.id = e.student_id
+WHERE
+    e.approved = 1
+;FROM
     course
 ;
 
